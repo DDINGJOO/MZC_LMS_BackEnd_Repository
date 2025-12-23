4c2e6f0f569b5c481df94e234753938636e4c5fb
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:11 +0900

    style: format AcademicTerm.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/academy/entity/AcademicTerm.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/academy/entity/AcademicTerm.java
@@ -1,16 +1,10 @@
 package com.mzc.backend.lms.domains.academy.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
@@ -22,22 +16,23 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "academic_terms")
 @Getter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AcademicTerm {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 학기 식별자
-    
-    @Column(name = "year", nullable = false)
-    private Integer year; // 학년도
-
-    @Column(name = "term_type", nullable = false)
-    private String termType; // 학기 구분 (1:봄학기/2:가을학기/SUMMER:여름학기/WINTER:겨울학기)
-
-    @Column(name = "start_date", nullable = false)
-    private LocalDate startDate; // 학기 시작일
-
-    @Column(name = "end_date", nullable = false)
-    private LocalDate endDate; // 학기 종료일
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 학기 식별자
+	
+	@Column(name = "year", nullable = false)
+	private Integer year; // 학년도
+	
+	@Column(name = "term_type", nullable = false)
+	private String termType; // 학기 구분 (1:봄학기/2:가을학기/SUMMER:여름학기/WINTER:겨울학기)
+	
+	@Column(name = "start_date", nullable = false)
+	private LocalDate startDate; // 학기 시작일
+	
+	@Column(name = "end_date", nullable = false)
+	private LocalDate endDate; // 학기 종료일
 }
