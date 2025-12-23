b46827804010ac986134d1fe8c691013999fc0cb
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:16 +0900

    style: format CourseScheduleRepository.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/repository/CourseScheduleRepository.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/repository/CourseScheduleRepository.java
@@ -3,6 +3,7 @@ package com.mzc.backend.lms.domains.course.course.repository;
 import com.mzc.backend.lms.domains.course.course.entity.CourseSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 /**
@@ -11,14 +12,14 @@ import java.util.Optional;
 
 @Repository
 public interface CourseScheduleRepository extends JpaRepository<CourseSchedule, Long> {
-    /**
-     * 강의 ID로 강의 스케줄 조회
-     */
-    Optional<CourseSchedule> findByCourseId(Long courseId);
-
-    /**
-     * 강의 ID로 강의 스케줄 존재 여부 확인
-     */
-    boolean existsByCourseId(Long courseId);
-
+	/**
+	 * 강의 ID로 강의 스케줄 조회
+	 */
+	Optional<CourseSchedule> findByCourseId(Long courseId);
+	
+	/**
+	 * 강의 ID로 강의 스케줄 존재 여부 확인
+	 */
+	boolean existsByCourseId(Long courseId);
+	
 }
