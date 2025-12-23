8ad670dec2a13426749d093d838553717be3cc91
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:11 +0900

    style: format RedisConfig.java

--- springboot/src/main/java/com/mzc/backend/lms/common/config/RedisConfig.java
+++ springboot/src/main/java/com/mzc/backend/lms/common/config/RedisConfig.java
@@ -11,20 +11,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        // String 직렬화 사용
-        StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        template.setKeySerializer(stringSerializer);
-        template.setValueSerializer(stringSerializer);
-        template.setHashKeySerializer(stringSerializer);
-        template.setHashValueSerializer(stringSerializer);
-
-        template.afterPropertiesSet();
-        return template;
-    }
+	
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		
+		// String 직렬화 사용
+		StringRedisSerializer stringSerializer = new StringRedisSerializer();
+		template.setKeySerializer(stringSerializer);
+		template.setValueSerializer(stringSerializer);
+		template.setHashKeySerializer(stringSerializer);
+		template.setHashValueSerializer(stringSerializer);
+		
+		template.afterPropertiesSet();
+		return template;
+	}
 }
