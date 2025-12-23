37e03bc01ac40efb7a28b95f3995a1025c5ae57b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:27 +0900

    style: format RefreshTokenCleanupSchedulerTest.java

--- springboot/src/test/java/com/mzc/backend/lms/domains/user/auth/token/scheduler/RefreshTokenCleanupSchedulerTest.java
+++ springboot/src/test/java/com/mzc/backend/lms/domains/user/auth/token/scheduler/RefreshTokenCleanupSchedulerTest.java
@@ -19,43 +19,43 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 @DisplayName("RefreshTokenCleanupScheduler 테스트")
 class RefreshTokenCleanupSchedulerTest {
-
-    @Mock
-    private RefreshTokenRepository refreshTokenRepository;
-
-    @InjectMocks
-    private RefreshTokenCleanupScheduler scheduler;
-
-    @Test
-    @DisplayName("만료/폐기 토큰 정리 스케줄러가 정상 실행된다")
-    void givenExpiredOrRevokedTokens_whenCleanup_thenTokensDeleted() {
-        // Given
-        given(refreshTokenRepository.deleteExpiredOrRevokedTokens(any(LocalDateTime.class)))
-                .willReturn(5);
-
-        // When
-        scheduler.cleanExpiredOrRevokedTokens();
-
-        // Then
-        ArgumentCaptor<LocalDateTime> captor = ArgumentCaptor.forClass(LocalDateTime.class);
-        verify(refreshTokenRepository).deleteExpiredOrRevokedTokens(captor.capture());
-
-        LocalDateTime capturedTime = captor.getValue();
-        assertThat(capturedTime).isNotNull();
-        assertThat(capturedTime).isBeforeOrEqualTo(LocalDateTime.now());
-    }
-
-    @Test
-    @DisplayName("삭제할 토큰이 없어도 정상 실행된다")
-    void givenNoTokensToDelete_whenCleanup_thenCompletesSuccessfully() {
-        // Given
-        given(refreshTokenRepository.deleteExpiredOrRevokedTokens(any(LocalDateTime.class)))
-                .willReturn(0);
-
-        // When
-        scheduler.cleanExpiredOrRevokedTokens();
-
-        // Then
-        verify(refreshTokenRepository).deleteExpiredOrRevokedTokens(any(LocalDateTime.class));
-    }
+	
+	@Mock
+	private RefreshTokenRepository refreshTokenRepository;
+	
+	@InjectMocks
+	private RefreshTokenCleanupScheduler scheduler;
+	
+	@Test
+	@DisplayName("만료/폐기 토큰 정리 스케줄러가 정상 실행된다")
+	void givenExpiredOrRevokedTokens_whenCleanup_thenTokensDeleted() {
+		// Given
+		given(refreshTokenRepository.deleteExpiredOrRevokedTokens(any(LocalDateTime.class)))
+				.willReturn(5);
+		
+		// When
+		scheduler.cleanExpiredOrRevokedTokens();
+		
+		// Then
+		ArgumentCaptor<LocalDateTime> captor = ArgumentCaptor.forClass(LocalDateTime.class);
+		verify(refreshTokenRepository).deleteExpiredOrRevokedTokens(captor.capture());
+		
+		LocalDateTime capturedTime = captor.getValue();
+		assertThat(capturedTime).isNotNull();
+		assertThat(capturedTime).isBeforeOrEqualTo(LocalDateTime.now());
+	}
+	
+	@Test
+	@DisplayName("삭제할 토큰이 없어도 정상 실행된다")
+	void givenNoTokensToDelete_whenCleanup_thenCompletesSuccessfully() {
+		// Given
+		given(refreshTokenRepository.deleteExpiredOrRevokedTokens(any(LocalDateTime.class)))
+				.willReturn(0);
+		
+		// When
+		scheduler.cleanExpiredOrRevokedTokens();
+		
+		// Then
+		verify(refreshTokenRepository).deleteExpiredOrRevokedTokens(any(LocalDateTime.class));
+	}
 }
