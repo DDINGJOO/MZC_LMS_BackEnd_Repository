64630b9096bc3ac6d28656764985f64135c742f7
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:13 +0900

    style: format PostCreateRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/board/dto/request/PostCreateRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/board/dto/request/PostCreateRequestDto.java
@@ -1,12 +1,12 @@
 package com.mzc.backend.lms.domains.board.dto.request;
 
-import java.util.List;
-
 import com.mzc.backend.lms.domains.board.enums.PostType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * 게시글 생성 요청 DTO
@@ -16,18 +16,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostCreateRequestDto {
-
-    private Long categoryId;
-
-    private String title;
-
-    private String content;
-
-    private PostType postType;
-
-    private Boolean isAnonymous;
-
-    private List<Long> attachmentIds;
-
-    private List<String> hashtags;
+	
+	private Long categoryId;
+	
+	private String title;
+	
+	private String content;
+	
+	private PostType postType;
+	
+	private Boolean isAnonymous;
+	
+	private List<Long> attachmentIds;
+	
+	private List<String> hashtags;
 }
