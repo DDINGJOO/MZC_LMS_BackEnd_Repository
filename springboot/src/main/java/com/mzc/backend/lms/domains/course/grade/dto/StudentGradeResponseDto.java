5e9afdda0d5f4e053b4b12cc0c92a459331c0588
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:17 +0900

    style: format StudentGradeResponseDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/grade/dto/StudentGradeResponseDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/grade/dto/StudentGradeResponseDto.java
@@ -9,21 +9,21 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class StudentGradeResponseDto {
-    private Long academicTermId;
-    private Long courseId;
-    private String courseName;
-    private Integer courseCredits;
-    private String status; // PUBLISHED
-
-    private BigDecimal midtermScore;
-    private BigDecimal finalExamScore;
-    private BigDecimal quizScore;
-    private BigDecimal assignmentScore;
-    private BigDecimal attendanceScore;
-
-    private BigDecimal finalScore;
-    private String finalGrade;
-    private LocalDateTime publishedAt;
+	private Long academicTermId;
+	private Long courseId;
+	private String courseName;
+	private Integer courseCredits;
+	private String status; // PUBLISHED
+	
+	private BigDecimal midtermScore;
+	private BigDecimal finalExamScore;
+	private BigDecimal quizScore;
+	private BigDecimal assignmentScore;
+	private BigDecimal attendanceScore;
+	
+	private BigDecimal finalScore;
+	private String finalGrade;
+	private LocalDateTime publishedAt;
 }
 
 
