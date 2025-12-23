8b28e1730274aaa9b55586d50149a08712322dd1
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:16 +0900

    style: format CourseStatusType.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/entity/CourseStatusType.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/entity/CourseStatusType.java
@@ -1,15 +1,10 @@
 package com.mzc.backend.lms.domains.course.course.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /*
     CourseStatusType 엔티티
@@ -19,16 +14,17 @@ import jakarta.persistence.Column;
 @Entity
 @Table(name = "course_status_types")
 @Getter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CourseStatusType {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "status_code", length = 20, unique = true, nullable = false)
-    private String statusCode; // 상태 코드 (ONGOING/COMPLETED/PENDING)"
-
-    @Column(name = "status_name", length = 50, nullable = false)
-    private String statusName; // 상태 이름 (진행중/완료/대기)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "status_code", length = 20, unique = true, nullable = false)
+	private String statusCode; // 상태 코드 (ONGOING/COMPLETED/PENDING)"
+	
+	@Column(name = "status_name", length = 50, nullable = false)
+	private String statusName; // 상태 이름 (진행중/완료/대기)
 }
