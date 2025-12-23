dd3b25113f1b38153c9f77f258990ec175cbd9ef
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:17 +0900

    style: format CourseNoticeUpdateRequest.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/notice/dto/request/CourseNoticeUpdateRequest.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/notice/dto/request/CourseNoticeUpdateRequest.java
@@ -9,14 +9,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CourseNoticeUpdateRequest {
-
-    @NotBlank(message = "제목은 필수입니다.")
-    @Size(max = 200, message = "제목은 200자 이내여야 합니다.")
-    private String title;
-
-    @NotBlank(message = "내용은 필수입니다.")
-    private String content;
-
-    @NotNull(message = "댓글 허용 여부는 필수입니다.")
-    private Boolean allowComments;
+	
+	@NotBlank(message = "제목은 필수입니다.")
+	@Size(max = 200, message = "제목은 200자 이내여야 합니다.")
+	private String title;
+	
+	@NotBlank(message = "내용은 필수입니다.")
+	private String content;
+	
+	@NotNull(message = "댓글 허용 여부는 필수입니다.")
+	private Boolean allowComments;
 }
