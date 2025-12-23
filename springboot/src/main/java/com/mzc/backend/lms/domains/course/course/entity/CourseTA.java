676bf0dfc7fdb65b4b6f98a972ee312c170e3bb0
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:16 +0900

    style: format CourseTA.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/course/course/entity/CourseTA.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/course/course/entity/CourseTA.java
@@ -1,55 +1,45 @@
 package com.mzc.backend.lms.domains.course.course.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.mzc.backend.lms.domains.user.student.entity.Student;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import com.mzc.backend.lms.domains.user.student.entity.Student;
-
 @Entity
 @Table(name = "course_tas")
 @Getter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CourseTA {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "course_id", nullable = false)
-    private Course course;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ta_id", nullable = false)
-    private Student ta;
-
-    @Column(name = "start_date", nullable = false)
-    private LocalDate startDate;
-
-    @Column(name = "end_date", nullable = false)
-    private LocalDate endDate;
-
-    @Column(name = "is_active", nullable = false)
-    private Boolean isActive;
-
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "course_id", nullable = false)
+	private Course course;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ta_id", nullable = false)
+	private Student ta;
+	
+	@Column(name = "start_date", nullable = false)
+	private LocalDate startDate;
+	
+	@Column(name = "end_date", nullable = false)
+	private LocalDate endDate;
+	
+	@Column(name = "is_active", nullable = false)
+	private Boolean isActive;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 }
