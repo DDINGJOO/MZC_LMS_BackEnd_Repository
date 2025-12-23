272eb45ef0a4becf496bfc215fe2254344e9ce33
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-23 13:47:13 +0900

    style: format AssignmentUpdateRequestDto.java

--- springboot/src/main/java/com/mzc/backend/lms/domains/board/assignment/dto/request/AssignmentUpdateRequestDto.java
+++ springboot/src/main/java/com/mzc/backend/lms/domains/board/assignment/dto/request/AssignmentUpdateRequestDto.java
@@ -16,26 +16,26 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AssignmentUpdateRequestDto {
-
-    // Post 정보
-    private String title;
-
-    private String content;
-
-    // Assignment 정보
-    private LocalDateTime dueDate;
-
-    private BigDecimal maxScore;
-
-    private String submissionMethod;
-
-    private Boolean lateSubmissionAllowed;
-
-    private BigDecimal latePenaltyPercent;
-
-    private Integer maxFileSizeMb;
-
-    private String allowedFileTypes;
-
-    private String instructions;
+	
+	// Post 정보
+	private String title;
+	
+	private String content;
+	
+	// Assignment 정보
+	private LocalDateTime dueDate;
+	
+	private BigDecimal maxScore;
+	
+	private String submissionMethod;
+	
+	private Boolean lateSubmissionAllowed;
+	
+	private BigDecimal latePenaltyPercent;
+	
+	private Integer maxFileSizeMb;
+	
+	private String allowedFileTypes;
+	
+	private String instructions;
 }
